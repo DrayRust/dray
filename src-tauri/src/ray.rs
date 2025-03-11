417d96a0b3b42a6e26e31f21868756b8da716435
Dray <dreambig9527@gmail.com>
2025-03-11 20:50:33 +0800

    changes

--- src-tauri/src/ray.rs
+++ src-tauri/src/ray.rs
@@ -4,27 +4,27 @@ use crate::dirs;
 
 pub fn start() -> bool {
 	let home_dir = dirs::get_home_dir().unwrap_or_else(|| {
-		error!("无法获取用户主目录");
+		error!("Failed to get user home directory");
 		std::path::PathBuf::from(".")
 	});
 	let ray_path = home_dir.join("dray").join("ray-bin").join("ray").to_str().unwrap_or("./ray-bin/ray");
 	let config_path = home_dir.join("dray").join("ray-bin").join("config.json").to_str().unwrap_or("./ray-bin/config.json");
 
 	if let Err(e) = command::start(ray_path, &["-c", config_path]) {
-		error!("启动 Ray Server 失败: {}", e);
+		error!("Failed to start Ray Server: {}", e);
 		false
 	} else {
-		info!("启动 Ray Server 成功");
+		info!("Ray Server started successfully");
 		true
 	}
 }
 
 pub fn stop() -> bool {
 	if let Err(e) = command::stop() {
-		error!("停止 Ray Server 失败: {}", e);
+		error!("Failed to stop Ray Server: {}", e);
 		false
 	} else {
-		info!("停止 Ray Server 成功");
+		info!("Ray Server stopped successfully");
 		true
 	}
 }
