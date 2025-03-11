af9c3bd5d22fb3ac4896447d176295f3248259db
Dray <dreambig9527@gmail.com>
2025-03-11 20:46:56 +0800

    changes

--- src-tauri/src/ray.rs
+++ src-tauri/src/ray.rs
@@ -1,8 +1,16 @@
 use logger::{error, info};
 use crate::command;
+use crate::dirs;
 
 pub fn start() -> bool {
-	if let Err(e) = command::start("./ray-bin/ray", &["-c", "./ray-bin/config.json"]) {
+	let home_dir = dirs::get_home_dir().unwrap_or_else(|| {
+		error!("无法获取用户主目录");
+		std::path::PathBuf::from(".")
+	});
+	let ray_path = home_dir.join("dray").join("ray-bin").join("ray").to_str().unwrap_or("./ray-bin/ray");
+	let config_path = home_dir.join("dray").join("ray-bin").join("config.json").to_str().unwrap_or("./ray-bin/config.json");
+
+	if let Err(e) = command::start(ray_path, &["-c", config_path]) {
 		error!("启动 Ray Server 失败: {}", e);
 		false
 	} else {
