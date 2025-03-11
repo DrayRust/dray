a40053c5a821feb2294128561b3f34ec8e0b94b5
Dray <dreambig9527@gmail.com>
2025-03-11 22:15:13 +0800

    changes

--- src-tauri/src/ray.rs
+++ src-tauri/src/ray.rs
@@ -1,4 +1,4 @@
-use logger::{error, info};
+use logger::{error, info, debug};
 use crate::command;
 use crate::dirs;
 
@@ -9,9 +9,11 @@ pub fn start() -> bool {
 	});
 	let ray_bin_path = home_dir.join("dray").join("ray-bin");
 	let ray_path: String = ray_bin_path.join("ray").to_str().unwrap_or("./ray-bin/ray").to_string();
-	let config_path: String = ray_bin_path.join("config.json").to_str().unwrap_or("./ray-bin/config.json").to_string();
+	let ray_config_path: String = ray_bin_path.join("config.json").to_str().unwrap_or("./ray-bin/config.json").to_string();
+	debug!("ray_path: {}", ray_path);
+	debug!("ray_config_path: {}", ray_config_path);
 
-	if let Err(e) = command::start(&*ray_path, &["-c", &*config_path]) {
+	if let Err(e) = command::start(&*ray_path, &["-c", &*ray_config_path]) {
 		error!("Failed to start Ray Server: {}", e);
 		false
 	} else {
