ba95b2876e83d2f393b926b29d5f2578a4fc7975
Dray <dreambig9527@gmail.com>
2025-03-11 21:07:01 +0800

    changes

--- src-tauri/src/ray.rs
+++ src-tauri/src/ray.rs
@@ -7,10 +7,11 @@ pub fn start() -> bool {
 		error!("Failed to get user home directory");
 		std::path::PathBuf::from(".")
 	});
-	let ray_path = home_dir.join("dray").join("ray-bin").join("ray").to_str().unwrap_or("./ray-bin/ray");
-	let config_path = home_dir.join("dray").join("ray-bin").join("config.json").to_str().unwrap_or("./ray-bin/config.json");
+	let ray_bin_path = home_dir.join("dray").join("ray-bin");
+	let ray_path: String = ray_bin_path.join("ray").to_str().unwrap_or("./ray-bin/ray").to_string();
+	let config_path: String = ray_bin_path.join("config.json").to_str().unwrap_or("./ray-bin/config.json").to_string();
 
-	if let Err(e) = command::start(ray_path, &["-c", config_path]) {
+	if let Err(e) = command::start(&*ray_path, &["-c", &*config_path]) {
 		error!("Failed to start Ray Server: {}", e);
 		false
 	} else {
