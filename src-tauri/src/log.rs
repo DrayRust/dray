15170c59e1c31247d76e0d9dfe70f1fec65ef6f7
Dray <dreambig9527@gmail.com>
2025-03-12 22:32:50 +0800

    changes

--- src-tauri/src/log.rs
+++ src-tauri/src/log.rs
@@ -1,11 +1,7 @@
+use crate::dirs;
 // Initialize log, set storage path
 pub fn init_log() {
-	let home_dir = dirs::home_dir().unwrap_or_else(|| {
-		eprintln!("Failed to get user home directory");
-		std::path::PathBuf::from(".")
-	});
-	let log_path = home_dir.join("dray").join("logs").join("dray.log");
-	logger::set_log_filepath(log_path.to_str().unwrap_or("logs/dray.log")).unwrap_or_else(|e| {
+	logger::set_log_filepath(dirs::get_dray_logs_dir().unwrap().join("dray.log").to_str().unwrap()).unwrap_or_else(|e| {
 		eprintln!("Failed to set log file path: {}", e);
 	});
 }
