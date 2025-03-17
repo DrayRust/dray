d75e5308e0f6dd6e1af6c4e681b90d0052283757
Dray <dreambig9527@gmail.com>
2025-03-17 13:07:08 +0800

    changes

--- src/page/Setting.tsx
+++ src/page/Setting.tsx
@@ -15,32 +15,44 @@ import {
 
 import { useTheme } from '../context/ThemeProvider'
 
-const Setting: React.FC = () => {
-    const {mode, toggleMode} = useTheme()
+import {
+    enable as autoStartEnable,
+    isEnabled as autoStartIsEnabled,
+    disable as autoStartDisable,
+} from '@tauri-apps/plugin-autostart'
 
+const Setting: React.FC = () => {
+    // 从上下文中获取当前主题模式和切换模式的函数
+    const {mode, toggleMode} = useTheme()
     const changeTheme = (newMode: string) => {
         toggleMode(newMode as 'light' | 'dark' | 'system')
     }
 
+    // 用于记录当前激活的选项卡索引，初始值为0（即第一个选项卡）
     const [activeTab, setActiveTab] = useState(0)
-
-    const handleTabChange = (_event: SyntheticEvent, newValue: number) => {
+    const changeTab = (_event: SyntheticEvent, newValue: number) => {
         setActiveTab(newValue)
+    }
+
+    // 用于记录当前开机自动启动的设置，初始值为false（即未设置自动启动）
+    const [autoStart, setAutoStart] = useState(false);
+    (async () => {
+        setAutoStart(await autoStartIsEnabled())
+    })()
+    const changeAutoStart = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        let checked = event.target.checked
+        setAutoStart(checked)
+        checked ? await autoStartEnable() : await autoStartDisable()
     }
 
     const invoke = (action: string) => {
         console.log(`Invoking action: ${action}`)
     }
 
-    const enableAutoStart = () => {
-        console.log('Enabling auto start')
-    }
-
-
     return (
         <Paper elevation={3} sx={{borderRadius: 2, overflow: 'visible'}}>
             <Paper elevation={1} sx={{alignItems: "center", borderRadius: '8px 8px 0 0'}}>
-                <Tabs value={activeTab} onChange={handleTabChange} aria-label="设置导航">
+                <Tabs value={activeTab} onChange={changeTab} aria-label="设置导航">
                     <Tab label="基本设置"/>
                     <Tab label="代理设置"/>
                     <Tab label="Web 设置"/>
@@ -64,7 +76,7 @@ const Setting: React.FC = () => {
                         <ListItemButton sx={{p: 0, cursor: 'default'}}>
                             <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{width: '100%', p: 1}}>
                                 <Typography variant="body1" sx={{paddingLeft: 1}}>开机启动</Typography>
-                                <Switch checked={false} onChange={enableAutoStart}/>
+                                <Switch checked={autoStart} onChange={changeAutoStart}/>
                             </Stack>
                         </ListItemButton>
                     </ListItem>
