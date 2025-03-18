aafeaf4b6d2294dd9041482eedcd015c98d9b4c3
Dray <dreambig9527@gmail.com>
2025-03-19 00:10:15 +0800

    changes

--- src-tauri/src/network/windows.rs
+++ src-tauri/src/network/windows.rs
@@ -1,57 +1,51 @@
-use super::execute_command;
+use super::command;
 use crate::config;
+
+const SETTINGS: &str = "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
 
 pub fn enable_auto_proxy() -> bool {
     let config = config::get_config();
     let url = format!("http://{}:{}/dray/proxy.js", config.web_server_host, config.web_server_port);
-    execute_command(&format!(
-        "reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v AutoConfigURL /t REG_SZ /d {} /f",
-        url
-    )) && execute_command("reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 1 /f")
+    command("reg", &["add", SETTINGS, "/v", "AutoConfigURL", "/t", "REG_SZ", "/d", &url, "/f"]).is_ok() &&
+        command("reg", &["add", SETTINGS, "/v", "ProxyEnable", "/t", "REG_DWORD", "/d", "1", "/f"]).is_ok()
 }
 
 pub fn enable_socks_proxy() -> bool {
     let config = config::get_config();
-    execute_command(&format!(
-        "reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyServer /t REG_SZ /d socks={}:{} /f",
-        config.ray_host, config.ray_socks_port
-    )) && execute_command("reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 1 /f")
+    command("reg", &["add", SETTINGS, "/v", "ProxyServer", "/t", "REG_SZ", "/d", &format!("socks={}:{}", config.ray_host, config.ray_socks_port), "/f"]).is_ok() &&
+        command("reg", &["add", SETTINGS, "/v", "ProxyEnable", "/t", "REG_DWORD", "/d", "1", "/f"]).is_ok()
 }
 
 pub fn enable_web_proxy() -> bool {
     let config = config::get_config();
-    execute_command(&format!(
-        "reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyServer /t REG_SZ /d http={}:{} /f",
-        config.ray_host, config.ray_http_port
-    )) && execute_command("reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 1 /f")
+    command("reg", &["add", SETTINGS, "/v", "ProxyServer", "/t", "REG_SZ", "/d", &format!("http={}:{}", config.ray_host, config.ray_http_port), "/f"]).is_ok() &&
+        command("reg", &["add", SETTINGS, "/v", "ProxyEnable", "/t", "REG_DWORD", "/d", "1", "/f"]).is_ok()
 }
 
 pub fn enable_secure_web_proxy() -> bool {
     let config = config::get_config();
-    execute_command(&format!(
-        "reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyServer /t REG_SZ /d https={}:{} /f",
-        config.ray_host, config.ray_http_port
-    )) && execute_command("reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 1 /f")
+    command("reg", &["add", SETTINGS, "/v", "ProxyServer", "/t", "REG_SZ", "/d", &format!("https={}:{}", config.ray_host, config.ray_http_port), "/f"]).is_ok() &&
+        command("reg", &["add", SETTINGS, "/v", "ProxyEnable", "/t", "REG_DWORD", "/d", "1", "/f"]).is_ok()
 }
 
 pub fn disable_auto_proxy() -> bool {
-    execute_command("reg delete \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v AutoConfigURL /f")
-        && execute_command("reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 0 /f")
+    command("reg", &["delete", SETTINGS, "/v", "AutoConfigURL", "/f"]).is_ok() &&
+        command("reg", &["add", SETTINGS, "/v", "ProxyEnable", "/t", "REG_DWORD", "/d", "0", "/f"]).is_ok()
 }
 
 pub fn disable_socks_proxy() -> bool {
-    execute_command("reg delete \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyServer /f")
-        && execute_command("reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 0 /f")
+    command("reg", &["delete", SETTINGS, "/v", "ProxyServer", "/f"]).is_ok() &&
+        command("reg", &["add", SETTINGS, "/v", "ProxyEnable", "/t", "REG_DWORD", "/d", "0", "/f"]).is_ok()
 }
 
 pub fn disable_web_proxy() -> bool {
-    execute_command("reg delete \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyServer /f")
-        && execute_command("reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 0 /f")
+    command("reg", &["delete", SETTINGS, "/v", "ProxyServer", "/f"]).is_ok() &&
+        command("reg", &["add", SETTINGS, "/v", "ProxyEnable", "/t", "REG_DWORD", "/d", "0", "/f"]).is_ok()
 }
 
 pub fn disable_secure_web_proxy() -> bool {
-    execute_command("reg delete \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyServer /f")
-        && execute_command("reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 0 /f")
+    command("reg", &["delete", SETTINGS, "/v", "ProxyServer", "/f"]).is_ok() &&
+        command("reg", &["add", SETTINGS, "/v", "ProxyEnable", "/t", "REG_DWORD", "/d", "0", "/f"]).is_ok()
 }
 
 pub fn disable_proxies() -> bool {
