fd70cf7813371964c3061afb1e49a9a8dccd64e6
Dray <dreambig9527@gmail.com>
2025-03-19 19:44:17 +0800

    changes

--- src/util/util.ts
+++ src/util/util.ts
@@ -5,3 +5,15 @@ export function debounce(func: Function, wait: number) {
         timeout = setTimeout(() => func.apply(this, args), wait)
     }
 }
+
+export const log = {
+    error: (message: string, ...args: any[]) => {
+        console.log(`[ERROR] ${message}`, ...args)
+    },
+    warn: (message: string, ...args: any[]) => {
+        console.log(`[WARN] ${message}`, ...args)
+    },
+    info: (message: string, ...args: any[]) => {
+        console.log(`[INFO] ${message}`, ...args)
+    },
+}
