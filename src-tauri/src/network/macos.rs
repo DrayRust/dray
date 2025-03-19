92285b107af9b6b18622eb903c2e93fb1b0b1e96
Dray <dreambig9527@gmail.com>
2025-03-19 10:31:52 +0800

    changes

--- src-tauri/src/network/macos.rs
+++ src-tauri/src/network/macos.rs
@@ -1,10 +1,9 @@
 use super::execute_command;
 use crate::config;
-
-/*
-// 获取当前使用的网络接口名称
 use std::process::Command;
-fn get_active_network_interface() -> Option<String> {
+
+// 获取当前使用的网络接口名称，如果没有获取到则返回 "Wi-Fi"
+fn get_active_network_interface() -> String {
     let output = Command::new("networksetup")
         .arg("-listallnetworkservices")
         .output()
@@ -17,56 +16,75 @@ fn get_active_network_interface() -> Option<String> {
             }
         });
 
-    output.and_then(|s| {
-        s.lines()
-            .skip(1) // 跳过第一行提示信息
-            .find(|line| !line.starts_with('*')) // 找到第一个未标记为禁用的接口
-            .map(|line| line.trim().to_string())
-    })
+    output
+        .and_then(|s| {
+            s.lines()
+                .skip(1) // 跳过第一行提示信息
+                .find(|line| !line.starts_with('*')) // 找到第一个未标记为禁用的接口
+                .map(|line| line.trim().to_string())
+        })
+        .unwrap_or_else(|| "Wi-Fi".to_string())
 }
-*/
 
 pub fn enable_auto_proxy() -> bool {
     let config = config::get_config();
+    let interface = get_active_network_interface();
     let url = format!("http://{}:{}/dray/proxy.js", config.web_server_host, config.web_server_port);
-    execute_command(&format!("networksetup -setautoproxyurl Wi-Fi {}", url))
-        && execute_command("networksetup -setautoproxystate Wi-Fi on")
+    execute_command(&format!("networksetup -setautoproxyurl {} {}", interface, url))
+        && execute_command(&format!("networksetup -setautoproxystate {} on", interface))
 }
 
 pub fn enable_socks_proxy() -> bool {
     let config = config::get_config();
-    execute_command(&format!("networksetup -setsocksfirewallproxy Wi-Fi {} {}", config.ray_host, config.ray_socks_port))
-        && execute_command("networksetup -setsocksfirewallproxystate Wi-Fi on")
+    let interface = get_active_network_interface();
+    execute_command(&format!(
+        "networksetup -setsocksfirewallproxy {} {} {}",
+        interface, config.ray_host, config.ray_socks_port
+    )) && execute_command(&format!("networksetup -setsocksfirewallproxystate {} on", interface))
 }
 
 pub fn enable_web_proxy() -> bool {
     let config = config::get_config();
-    execute_command(&format!("networksetup -setwebproxy Wi-Fi {} {}", config.ray_host, config.ray_http_port))
-        && execute_command("networksetup -setwebproxystate Wi-Fi on")
+    let interface = get_active_network_interface();
+    execute_command(&format!(
+        "networksetup -setwebproxy {} {} {}",
+        interface, config.ray_host, config.ray_http_port
+    )) && execute_command(&format!("networksetup -setwebproxystate {} on", interface))
 }
 
 pub fn enable_secure_web_proxy() -> bool {
     let config = config::get_config();
-    execute_command(&format!("networksetup -setsecurewebproxy Wi-Fi {} {}", config.ray_host, config.ray_http_port))
-        && execute_command("networksetup -setsecurewebproxystate Wi-Fi on")
+    let interface = get_active_network_interface();
+    execute_command(&format!(
+        "networksetup -setsecurewebproxy {} {} {}",
+        interface, config.ray_host, config.ray_http_port
+    )) && execute_command(&format!("networksetup -setsecurewebproxystate {} on", interface))
 }
 
 pub fn disable_auto_proxy() -> bool {
-    execute_command("networksetup -setautoproxystate Wi-Fi off")
+    let interface = get_active_network_interface();
+    execute_command(&format!("networksetup -setautoproxystate {} off", interface))
 }
 
 pub fn disable_socks_proxy() -> bool {
-    execute_command("networksetup -setsocksfirewallproxystate Wi-Fi off")
+    let interface = get_active_network_interface();
+    execute_command(&format!("networksetup -setsocksfirewallproxystate {} off", interface))
 }
 
 pub fn disable_web_proxy() -> bool {
-    execute_command("networksetup -setwebproxystate Wi-Fi off")
+    let interface = get_active_network_interface();
+    execute_command(&format!("networksetup -setwebproxystate {} off", interface))
 }
 
 pub fn disable_secure_web_proxy() -> bool {
-    execute_command("networksetup -setsecurewebproxystate Wi-Fi off")
+    let interface = get_active_network_interface();
+    execute_command(&format!("networksetup -setsecurewebproxystate {} off", interface))
 }
 
 pub fn disable_proxies() -> bool {
-    disable_auto_proxy() && disable_socks_proxy() && disable_web_proxy() && disable_secure_web_proxy()
+    let interface = get_active_network_interface();
+    execute_command(&format!("networksetup -setautoproxystate {} off", interface))
+        && execute_command(&format!("networksetup -setsocksfirewallproxystate {} off", interface))
+        && execute_command(&format!("networksetup -setwebproxystate {} off", interface))
+        && execute_command(&format!("networksetup -setsecurewebproxystate {} off", interface))
 }
