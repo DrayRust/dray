975c473076313b378ce3595b70614420fd4ecbbd
Dray <dreambig9527@gmail.com>
2025-03-21 11:37:48 +0800

    changes

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,9 +1,33 @@
-export function debounce(func: Function, wait: number) {
+import { useRef, useEffect } from 'react'
+
+export function debounce<T extends (...args: any[]) => any>(func: T, wait: number) {
     let timeout: ReturnType<typeof setTimeout> | null = null
-    return function (this: any, ...args: any[]) {
+
+    const debounced = function (this: any, ...args: Parameters<T>) {
+        const later = () => {
+            timeout = null
+            func.apply(this, args)
+        }
         if (timeout) clearTimeout(timeout)
-        timeout = setTimeout(() => func.apply(this, args), wait)
+        timeout = setTimeout(later, wait)
     }
+
+    debounced.cancel = () => {
+        if (timeout) {
+            clearTimeout(timeout)
+            timeout = null
+        }
+    }
+
+    return debounced
+}
+
+export const useDebounce = <T extends (...args: any[]) => any>(callback: T, delay: number) => {
+    const debouncedCallback = useRef(debounce(callback, delay))
+    useEffect(() => {
+        return () => debouncedCallback.current.cancel()
+    }, [callback, delay])
+    return debouncedCallback.current
 }
 
 export function validateIp(value: string) {
