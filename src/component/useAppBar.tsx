9959624dbf65f61dfb46dfb6f12940f985fc36a7
Dray <drayrust123@gmail.com>
2025-04-01 14:25:58 +0800

    changes

--- src/component/useAppBar.tsx
+++ src/component/useAppBar.tsx
@@ -6,7 +6,8 @@ export const useAppBar = (backUrl: string, title: string) => {
     const navigate = useNavigate()
     const AppBarComponent = () => (
         <AppBar position="static" sx={{p: 1, pl: 2, display: 'flex', flexDirection: 'row', justifyContent: "flex-start", alignItems: "center"}}>
-            <Button variant="outlined" startIcon={<ArrowBackIosNewIcon/>} onClick={() => navigate(backUrl || '/')} sx={{mr: 2}}>返回</Button>
+            <Button variant="outlined" color="warning" startIcon={<ArrowBackIosNewIcon/>} sx={{mr: 2}}
+                    onClick={() => navigate(backUrl || '/')}>返回</Button>
             <Typography variant="body1">{title}</Typography>
         </AppBar>
     )
