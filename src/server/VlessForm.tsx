7c47d55301df585cc266b4dbb208441158c8e6a7
Dray <drayrust123@gmail.com>
2025-04-04 23:38:18 +0800

    changes

--- src/server/VlessForm.tsx
+++ src/server/VlessForm.tsx
@@ -39,7 +39,7 @@ export const VlessForm = ({form, errors, handleChange, setFormData}: VlessFormPr
                 onChange={handleChange}/>
         </Grid>
         <Grid size={12}>
-            <Stack direction="row" spacing={1} alignItems="flex-start">
+            <Stack direction="row" spacing={2} alignItems="flex-start">
                 <TextField
                     sx={{flex: 1}}
                     fullWidth size="small"
