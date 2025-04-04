e8f14915837c8a4786bed43efe12dadc69ccf8e6
Dray <drayrust123@gmail.com>
2025-04-04 17:57:12 +0800

    changes

--- src/util/util.ts
+++ src/util/util.ts
@@ -32,6 +32,11 @@ export function generateUUID(): string {
     })
 }
 
+export function isValidUUID(uuid: string): boolean {
+    const regex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+    return regex.test(uuid)
+}
+
 /**
  * 计算字符串的哈希值
  * @param input 输入字符串
