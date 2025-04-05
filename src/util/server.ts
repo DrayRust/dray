c30341e9b60cff2febe34baf75b7dc0d558a2358
Dray <drayrust123@gmail.com>
2025-04-05 13:03:14 +0800

    changes

--- src/util/server.ts
+++ src/util/server.ts
@@ -36,8 +36,11 @@ export async function uriToServerRow(uri: string): Promise<ServerRow | null> {
 }
 
 /**
- * PS: 设计的乱七八糟，人都看懵逼，完全不想兼容，encryption 为什么不简化成 enc ？
  * VMess / VLESS 分享链接提案: https://github.com/XTLS/Xray-core/discussions/716
+ *
+ * PS: VMess + VLESS 混在一起，人都看傻了，完全不想兼容。[设计目标：导入兼容一下，导出不考虑兼容别的软件，以简单为主]
+ * 单词有些简化，有些不简化，简直乱七八糟，encryption 为什么不简化为 enc ？
+ * 想区分 security 和 encryption，又因为历史问题，配置文件和分享链接，完全变两套规则，把人搞的更懵逼。
  *
  * # VMess + TCP，不加密（仅作示例，不安全）
  * vmess://99c80931-f3f1-4f84-bffd-6eed6030f53d@qv2ray.net:31415?encryption=none#VMessTCPNaked
@@ -227,8 +230,8 @@ async function uriToSsRow(uri: string): Promise<ServerRow> {
         data = {
             add: url.hostname,
             port: Number(url.port) || 0,
-            scy: method || 'aes-256-gcm',
-            pwd: password || ''
+            pwd: password || '',
+            scy: method || '',
         }
     } else {
         const base64 = uri.replace('ss://', '')
@@ -238,8 +241,8 @@ async function uriToSsRow(uri: string): Promise<ServerRow> {
         data = {
             add: d.add,
             port: Number(d.port) || 0,
-            scy: d.scy || 'aes-256-gcm',
-            pwd: d.pwd || ''
+            pwd: d.pwd || '',
+            scy: d.scy || '',
         }
     }
 
@@ -306,10 +309,10 @@ export function serverRowToUri(row: ServerRow): string {
     const {type, data, ps} = row
     try {
         switch (type) {
-            case 'vless':
-                return vlessRowToUri(data as VlessRow, ps)
             case 'vmess':
                 return vmessRowToUri(data as VmessRow, ps)
+            case 'vless':
+                return vlessRowToUri(data as VlessRow, ps)
             case 'ss':
                 return ssRowToUri(data as SsRow, ps)
             case 'trojan':
@@ -324,32 +327,6 @@ export function serverRowToUri(row: ServerRow): string {
     }
 }
 
-function vlessRowToUri(row: VlessRow, ps: string): string {
-    const url = new URL('vless://')
-    url.hostname = row.add
-    url.port = row.port.toString()
-    url.username = row.id
-    url.hash = ps ? `#${ps}` : ''
-
-    const p = new URLSearchParams()
-    if (row.flow) p.set('flow', row.flow)
-    if (row.encryption) p.set('encryption', row.encryption)
-    if (row.type) p.set('type', row.type)
-    if (row.host) p.set('host', row.host)
-    if (row.path) p.set('path', row.path)
-    if (row.fp) p.set('fp', row.fp)
-    if (row.pbk) p.set('pbk', row.pbk)
-    if (row.sid) p.set('sid', row.sid)
-    if (row.sni) p.set('sni', row.sni)
-    if (row.serviceName) p.set('serviceName', row.serviceName)
-    if (row.headerType) p.set('headerType', row.headerType)
-    if (row.seed) p.set('seed', row.seed)
-    if (row.mode) p.set('mode', row.mode)
-
-    url.search = p.toString()
-    return url.toString()
-}
-
 function vmessRowToUri(row: VmessRow, ps: string): string {
     const url = new URL('vmess://')
     url.hostname = row.add
@@ -359,16 +336,51 @@ function vmessRowToUri(row: VmessRow, ps: string): string {
 
     const p = new URLSearchParams()
     if (row.aid) p.set('aid', row.aid.toString())
-    if (row.alpn) p.set('alpn', row.alpn)
-    if (row.sni) p.set('sni', row.sni)
+
     if (row.net) p.set('net', row.net)
+    if (row.scy) p.set('scy', row.scy)
+
     if (row.host) p.set('host', row.host)
     if (row.path) p.set('path', row.path)
-    if (row.tls) p.set('tls', row.tls)
-    if (row.fp) p.set('fp', row.fp)
+
     if (row.type) p.set('type', row.type)
     if (row.seed) p.set('seed', row.seed)
     if (row.mode) p.set('mode', row.mode)
+
+    if (row.tls) p.set('tls', row.tls.toString())
+    if (row.alpn) p.set('alpn', row.alpn)
+    if (row.fp) p.set('fp', row.fp)
+
+    url.search = p.toString()
+    return url.toString()
+}
+
+function vlessRowToUri(row: VlessRow, ps: string): string {
+    const url = new URL('vless://')
+    url.hostname = row.add
+    url.port = row.port.toString()
+    url.username = row.id
+    url.hash = ps ? `#${ps}` : ''
+
+    const p = new URLSearchParams()
+    if (row.net) p.set('net', row.net)
+    if (row.scy) p.set('scy', row.scy)
+
+    if (row.host) p.set('host', row.host)
+    if (row.path) p.set('path', row.path)
+    if (row.sni) p.set('sni', row.sni)
+
+    if (row.mode) p.set('mode', row.mode)
+    if (row.extra) p.set('extra', row.extra)
+
+    if (row.alpn) p.set('alpn', row.alpn)
+    if (row.fp) p.set('fp', row.fp)
+
+    if (row.flow) p.set('flow', row.flow)
+
+    if (row.pbk) p.set('pbk', row.pbk)
+    if (row.sid) p.set('sid', row.sid)
+    if (row.spx) p.set('spx', row.spx)
 
     url.search = p.toString()
     return url.toString()
@@ -378,7 +390,7 @@ function ssRowToUri(row: SsRow, ps: string): string {
     const url = new URL('ss://')
     url.hostname = row.add
     url.port = row.port.toString()
-    url.username = `${row.scy}:${row.pwd}`
+    url.username = encodeBase64(`${row.scy}:${row.pwd}`)
     url.hash = ps ? `#${ps}` : ''
     return url.toString()
 }
@@ -391,9 +403,13 @@ function trojanRowToUri(row: TrojanRow, ps: string): string {
     url.hash = ps ? `#${ps}` : ''
 
     const p = new URLSearchParams()
-    if (row.flow) p.set('flow', row.flow)
-    if (row.sni) p.set('sni', row.sni)
-    if (row.fp) p.set('fp', row.fp)
+    p.set('encryption', 'none')
+    p.set('security', 'tls')
+    p.set('type', row.net || 'grpc')
+
+    if (row.host) p.set('host', row.host)
+    if (row.path) p.set('path', row.path)
+    if (row.sni) p.set('serviceName', row.sni)
 
     url.search = p.toString()
     return url.toString()
@@ -403,10 +419,10 @@ export function serverRowToBase64Uri(row: ServerRow): string {
     const {type, data, ps} = row
     try {
         switch (type) {
-            case 'vless':
-                return vlessRowToBase64Uri(data as VlessRow, ps)
             case 'vmess':
                 return vmessRowToBase64Uri(data as VmessRow, ps)
+            case 'vless':
+                return vlessRowToBase64Uri(data as VlessRow, ps)
             case 'ss':
                 return ssRowToBase64Uri(data as SsRow, ps)
             case 'trojan':
@@ -421,14 +437,14 @@ export function serverRowToBase64Uri(row: ServerRow): string {
     }
 }
 
-function vlessRowToBase64Uri(row: VlessRow, ps: string): string {
-    const data = {ps, ...row}
-    return `vless://${encodeBase64(JSON.stringify(data))}`
-}
-
 function vmessRowToBase64Uri(row: VmessRow, ps: string): string {
     const data = {ps, v: 2, ...row}
     return `vmess://${encodeBase64(JSON.stringify(data))}`
+}
+
+function vlessRowToBase64Uri(row: VlessRow, ps: string): string {
+    const data = {ps, ...row}
+    return `vless://${encodeBase64(JSON.stringify(data))}`
 }
 
 function ssRowToBase64Uri(row: SsRow, ps: string): string {
@@ -445,10 +461,10 @@ export function serverRowToConf(row: ServerRow): any {
     const {type, data} = row
     try {
         switch (type) {
-            case 'vless':
-                return vlessRowToConf(data as VlessRow)
             case 'vmess':
                 return vmessRowToConf(data as VmessRow)
+            case 'vless':
+                return vlessRowToConf(data as VlessRow)
             case 'ss':
                 return ssRowToConf(data as SsRow)
             case 'trojan':
@@ -460,37 +476,6 @@ export function serverRowToConf(row: ServerRow): any {
     } catch (e) {
         log.error("Error converting server row to config:", e)
         return null
-    }
-}
-
-function vlessRowToConf(row: VlessRow): any {
-    return {
-        tag: "proxy",
-        protocol: "vless",
-        settings: {
-            vnext: [
-                {
-                    address: row.add,
-                    port: row.port,
-                    users: [
-                        {
-                            id: row.id,
-                            encryption: row.encryption || 'none',
-                            flow: row.flow || ''
-                        }
-                    ]
-                }
-            ]
-        },
-        streamSettings: {
-            network: row.net || 'tcp',
-            security: row.scy || 'none',
-            realitySettings: {
-                serverName: row.sni || '',
-                publicKey: row.pbk || '',
-                fingerprint: row.fp || 'chrome'
-            }
-        }
     }
 }
 
@@ -517,13 +502,44 @@ function vmessRowToConf(row: VmessRow): any {
             network: row.net || 'tcp',
             security: row.tls || 'none',
             tlsSettings: {
-                serverName: row.sni || '',
+                serverName: row.path || '',
             },
             wsSettings: {
                 path: row.path || '',
                 headers: {
                     Host: row.host || ''
                 }
+            }
+        }
+    }
+}
+
+function vlessRowToConf(row: VlessRow): any {
+    return {
+        tag: "proxy",
+        protocol: "vless",
+        settings: {
+            vnext: [
+                {
+                    address: row.add,
+                    port: row.port,
+                    users: [
+                        {
+                            id: row.id,
+                            encryption: 'none',
+                            flow: row.flow || ''
+                        }
+                    ]
+                }
+            ]
+        },
+        streamSettings: {
+            network: row.net || 'tcp',
+            security: row.scy || 'none',
+            realitySettings: {
+                serverName: row.sni || '',
+                publicKey: row.pbk || '',
+                fingerprint: row.fp || 'chrome'
             }
         }
     }
