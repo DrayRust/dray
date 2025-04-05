f9f9033dc4dc6e5acebc5369accaf54cf5f701b2
Dray <drayrust123@gmail.com>
2025-04-05 14:28:27 +0800

    changes

--- src/util/server.ts
+++ src/util/server.ts
@@ -222,7 +222,7 @@ async function uriToSsRow(uri: string): Promise<ServerRow> {
     let ps = ''
     let data: SsRow
 
-    if (url.hostname) {
+    if (url.username) {
         if (url.hash) ps = url.hash.slice(1).trim()
         const [method, password] = decodeBase64(decodeURIComponent(url.username)).split(':')
         data = {
