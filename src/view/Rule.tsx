ae3cd95c44d8c59964ab3f8f70504a1fe7cabd02
Dray <drayrust123@gmail.com>
2025-04-10 10:29:15 +0800

    changes

--- src/view/Rule.tsx
+++ src/view/Rule.tsx
@@ -46,9 +46,19 @@ const Rule: React.FC<NavProps> = ({setNavState}) => {
         setRuleConfig(prev => ({...prev, globalProxy: !prev.globalProxy}))
     }
 
+    const handleDomainChange = (type: keyof RuleDomain) => (e: React.ChangeEvent<HTMLInputElement>) => {
+        setRuleDomain(prev => ({...prev, [type]: e.target.value}))
+    }
+
     const [showSuccess, setShowSuccess] = useState(false)
     const handleSaveRuleDomain = useDebounce(() => {
-        saveRuleDomain(ruleDomain).then(() => {
+        const newRuleDomain = {
+            proxy: ruleDomain.proxy.trim(),
+            direct: ruleDomain.direct.trim(),
+            block: ruleDomain.block.trim()
+        }
+        setRuleDomain(newRuleDomain)
+        saveRuleDomain(newRuleDomain).then(() => {
             setShowSuccess(true)
             setTimeout(() => setShowSuccess(false), 1000)
         }).catch(_ => 0)
@@ -67,7 +77,7 @@ const Rule: React.FC<NavProps> = ({setNavState}) => {
                     <ToggleButton value="dns"><DnsIcon sx={{mr: 1}}/>DNS 规则</ToggleButton>
                 </ToggleButtonGroup>
             </div>
-            <Paper elevation={3} sx={{p: 2, maxWidth: '800px', m: 'auto', overflow: 'auto'}}>
+            <Card sx={{p: 2, maxWidth: '800px', maxHeight: 'calc(100% - 56px)', m: 'auto', overflow: 'auto'}}>
                 {ruleMode === 'route' && (<Stack spacing={2}>
                     <div className="flex-between">
                         <Typography variant="body1" sx={{pl: 1}}>全局代理</Typography>
@@ -77,38 +87,47 @@ const Rule: React.FC<NavProps> = ({setNavState}) => {
                         <BottomNavigation
                             sx={{mb: 2}}
                             showLabels
-                            component={Card}
+                            component={Paper}
+                            elevation={5}
                             value={ruleType}
                             onChange={(_, v) => setRuleType(v)}>
                             <BottomNavigationAction label="代理" icon={<SendIcon/>}/>
                             <BottomNavigationAction label="直连" icon={<FlightIcon/>}/>
                             <BottomNavigationAction label="阻止" icon={<BlockIcon/>}/>
                         </BottomNavigation>
+
                         {ruleType === 0 ? (<>
                             <Alert variant="filled" severity="warning">
                                 通过第三方服务器访问网络，适合访问国外网站或需要隐藏真实 IP 的场景。比如访问 Google、YouTube 等。
                             </Alert>
                             <TextField
-                                variant="outlined" label="请输入域名" fullWidth multiline rows={10}
-                                value={ruleDomain.proxy} onChange={(e) => setRuleDomain({...ruleDomain, proxy: e.target.value})}
-                                placeholder="每行一条，例如：google.com"/>
+                                variant="outlined" fullWidth multiline rows={6}
+                                label="请填写域名(每行一条)"
+                                value={ruleDomain.proxy}
+                                onChange={handleDomainChange('proxy')}
+                                placeholder="例如：google.com"/>
                         </>) : ruleType === 1 ? (<>
                             <Alert variant="filled" severity="success">
                                 直接连接网络，不经过任何代理服务器，适合访问国内网站或不需要加速的场景。比如访问百度、淘宝等。
                             </Alert>
                             <TextField
-                                variant="outlined" label="请输入域名" fullWidth multiline rows={10}
-                                value={ruleDomain.direct} onChange={(e) => setRuleDomain({...ruleDomain, direct: e.target.value})}
-                                placeholder="每行一条，例如：baidu.com"/>
+                                variant="outlined" fullWidth multiline rows={6}
+                                label="请填写域名(每行一条)"
+                                value={ruleDomain.direct}
+                                onChange={handleDomainChange('direct')}
+                                placeholder="例如：baidu.com"/>
                         </>) : ruleType === 2 && (<>
                             <Alert variant="filled" severity="error">
                                 阻止访问某些网站或服务，适合屏蔽广告、恶意网站或不希望访问的内容。
                             </Alert>
                             <TextField
-                                variant="outlined" label="请输入域名" fullWidth multiline rows={10}
-                                value={ruleDomain.block} onChange={(e) => setRuleDomain({...ruleDomain, block: e.target.value})}
-                                placeholder="每行一条，例如：360.cn"/>
+                                variant="outlined" fullWidth multiline rows={6}
+                                label="请填写域名(每行一条)"
+                                value={ruleDomain.block}
+                                onChange={handleDomainChange('block')}
+                                placeholder="例如：360.cn"/>
                         </>)}
+
                         <div className="flex-between">
                             <Stack direction="row" spacing={2} sx={{justifyContent: "flex-start", alignItems: "center"}}>
                                 <Button variant="contained" color="info" onClick={handleSaveRuleDomain}>确认</Button>
@@ -121,7 +140,7 @@ const Rule: React.FC<NavProps> = ({setNavState}) => {
 
                 {ruleMode === 'dns' && (<>
                 </>)}
-            </Paper>
+            </Card>
         </Paper>
         <RuleAdvanced open={open} setOpen={setOpen}/>
     </>)
