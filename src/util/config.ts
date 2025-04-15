44ca3c7d12c07fb66cf8310db4af34033304ae04
Dray <drayrust123@gmail.com>
2025-04-15 19:06:58 +0800

    changes

--- src/util/config.ts
+++ src/util/config.ts
@@ -62,40 +62,16 @@ export const DEFAULT_RULE_MODE_LIST: RuleModeList = [
     {
         "name": "中国大陆模式",
         "note": "专为中国大陆网络环境优化的访问规则",
-        "domainStrategy": 'IPIfNonMatch',
-        "hash": "e7600b7f166e9726297e58cbe54ba431a726f85eb061ed5fca7f4577eccf670a",
+        "domainStrategy": "IPIfNonMatch",
+        "hash": "42e1dff7eafdd5c1b5f399bf178715881ad9bf0203705f9c41e75ddf57c84abc",
         "rules": [
             {
                 "name": "中国大陆 DNS 服务器",
                 "note": "排除中国大陆常用 DNS 服务器",
                 "outboundTag": "direct",
-                "ruleType": "ip",
-                "domain": "",
-                "ip": "1.12.12.12\n1.2.4.8\n101.226.4.6\n101.6.6.6\n114.114.114.110\n114.114.114.114\n114.114.114.119\n114.114.115.110\n114.114.115.115\n114.114.115.119\n117.50.22.22\n119.29.29.29\n123.125.81.6\n140.207.198.6\n180.76.76.76\n182.254.116.116\n2001:da8:202:10::36\n202.96.128.86\n202.96.134.33\n210.2.4.8\n211.136.192.6\n211.136.192.7\n218.30.118.6\n223.5.5.5\n223.6.6.6\n2400:3200::1\n2400:3200:baba::1\n2408:8899::1\n2409:8088::1\n240e:4c:4008::1\n52.80.66.66",
-                "port": "",
-                "sourcePort": "",
-                "network": "",
-                "protocol": ""
-            },
-            {
-                "name": "中国大陆 DNS 服务器（域名）",
-                "note": "排除中国大陆常用 DNS 服务器（域名）",
-                "outboundTag": "direct",
-                "ruleType": "domain",
+                "ruleType": "multi",
                 "domain": "domain:114dns.com\ndomain:360.cn\ndomain:alidns.com\ndomain:chinamobile.com\ndomain:chinatelecom.com.cn\ndomain:chinaunicom.com\ndomain:cnnic.cn\ndomain:dns.360.cn\ndomain:dns.alidns.com\ndomain:dns.baidu.com\ndomain:dnspod.cn\ndomain:dnspod.com\ndomain:doh.360.cn\ndomain:doh.pub\ndomain:dot.360.cn\ndomain:dot.pub\ndomain:onedns.net\ndomain:tsinghua.edu.cn",
-                "ip": "",
-                "port": "",
-                "sourcePort": "",
-                "network": "",
-                "protocol": ""
-            },
-            {
-                "name": "常用 Google 域名",
-                "note": "代理常用 Google 域名，精准匹配域名，性能能快那么一点",
-                "outboundTag": "proxy",
-                "ruleType": "domain",
-                "domain": "domain:google-analytics.com\ndomain:google-analytics.com.cn\ndomain:google.com\ndomain:googleadapis.com\ndomain:googleadservices.com\ndomain:googleadservices.com.cn\ndomain:googleapis.cn\ndomain:googleapis.com\ndomain:googletagmanager.com\ndomain:googletagservices.com\ndomain:googleusercontent.com\ndomain:gstatic.com",
-                "ip": "",
+                "ip": "1.12.12.12\n1.2.4.8\n101.226.4.6\n101.6.6.6\n114.114.114.110\n114.114.114.114\n114.114.114.119\n114.114.115.110\n114.114.115.115\n114.114.115.119\n117.50.22.22\n119.29.29.29\n123.125.81.6\n140.207.198.6\n180.76.76.76\n182.254.116.116\n2001:da8:202:10::36\n202.96.128.86\n202.96.134.33\n210.2.4.8\n211.136.192.6\n211.136.192.7\n218.30.118.6\n223.5.5.5\n223.6.6.6\n2400:3200::1\n2400:3200:baba::1\n2408:8899::1\n2409:8088::1\n240e:4c:4008::1\n52.80.66.66",
                 "port": "",
                 "sourcePort": "",
                 "network": "",
@@ -190,7 +166,7 @@ export const DEFAULT_RULE_MODE_LIST: RuleModeList = [
     {
         "name": "俄罗斯模式",
         "note": "专为俄罗斯网络环境优化的访问规则",
-        "domainStrategy": 'IPIfNonMatch',
+        "domainStrategy": "IPIfNonMatch",
         "hash": "144ea270d77f516f60138522620149c521d8fa72c7cff7fcdefc82ad73bb97ae",
         "rules": [
             {
@@ -270,21 +246,9 @@ export const DEFAULT_RULE_MODE_LIST: RuleModeList = [
     {
         "name": "伊朗模式",
         "note": "专为伊朗网络环境优化的访问规则",
-        "domainStrategy": 'IPIfNonMatch',
-        "hash": "740b450642f85c0e78ccd15810ced1fedefb865f66bef1b168b4e52a8fda3558",
+        "domainStrategy": "IPIfNonMatch",
+        "hash": "8e894a2a484f893b2314be1d1d00435d57ab442a00caa9b3a911d14883ccce7c",
         "rules": [
-            {
-                "name": "常用 Google 域名",
-                "note": "代理常用 Google 域名",
-                "outboundTag": "proxy",
-                "ruleType": "domain",
-                "domain": "domain:google-analytics.com\ndomain:google-analytics.com.cn\ndomain:google.com\ndomain:googleadapis.com\ndomain:googleadservices.com\ndomain:googleadservices.com.cn\ndomain:googleapis.cn\ndomain:googleapis.com\ndomain:googletagmanager.com\ndomain:googletagservices.com\ndomain:googleusercontent.com\ndomain:gstatic.com",
-                "ip": "",
-                "port": "",
-                "sourcePort": "",
-                "network": "",
-                "protocol": ""
-            },
             {
                 "name": "UDP 443 流量",
                 "note": "屏蔽 UDP 443 端口流量，部分游戏，流媒体会用这个端口",
