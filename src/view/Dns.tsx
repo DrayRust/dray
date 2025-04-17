1dbda33c5b6eb4e636961b16a04cea457b563979
Dray <drayrust123@gmail.com>
2025-04-17 17:37:40 +0800

    changes

--- src/view/Dns.tsx
+++ src/view/Dns.tsx
@@ -1,16 +1,44 @@
 import { useState, useEffect } from 'react'
 import {
-    Alert, Stack, BottomNavigation, BottomNavigationAction, Paper,
+    Alert, Button, Chip, Card, Checkbox, Dialog, Stack, BottomNavigation, BottomNavigationAction, Paper,
+    TableContainer, Table, TableRow, TableCell, TableBody, Tooltip, IconButton,
     Typography, Switch, TextField, MenuItem,
 } from '@mui/material'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import SettingsIcon from '@mui/icons-material/Settings'
 import TuneIcon from '@mui/icons-material/Tune'
 import ListIcon from '@mui/icons-material/List'
+import AddIcon from '@mui/icons-material/Add'
+import FileUploadIcon from '@mui/icons-material/FileUpload'
+import FileDownloadIcon from '@mui/icons-material/FileDownload'
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import DeleteIcon from '@mui/icons-material/Delete'
 
 import { DnsTable } from "./DnsTable.tsx"
-import { readDnsConfig, readDnsModeList } from "../util/invoke.ts"
+import { readDnsConfig, readDnsModeList, saveDnsModeList } from "../util/invoke.ts"
+import { ErrorCard, LoadingCard } from "../component/useCard.tsx"
+import { decodeBase64, encodeBase64, hashJson, safeJsonParse } from "../util/crypto.ts"
+import { useAlertDialog } from "../component/useAlertDialog.tsx"
+import { clipboardWriteText } from "../util/tauri.ts"
+import { useDialog } from "../component/useDialog.tsx"
+
+const DEFAULT_DNS_MODE_ROW: DnsModeRow = {
+    name: '',
+    note: '',
+    tag: '',
+    hash: '',
+    hosts: [],
+    servers: [],
+    clientIP: '',
+    queryStrategy: '',
+    disableCache: false,
+    disableFallback: false,
+    disableFallbackIfMatch: false,
+}
 
 export const Dns = () => {
+    const [loading, setLoading] = useState(true)
     const [dnsNav, setDnsNav] = useState(0)
     const [dnsConfig, setDnsConfig] = useState<DnsConfig>({
         enable: false,
@@ -24,6 +52,7 @@ export const Dns = () => {
 
             const modeList = await readDnsModeList() as DnsModeList
             if (modeList) setDnsModeList(modeList)
+            setLoading(false)
         })()
     }, [])
 
@@ -35,7 +64,193 @@ export const Dns = () => {
         setDnsConfig({...dnsConfig, mode: Number(e.target.value)})
     }
 
+    const [action, setAction] = useState('')
+    const [row, setRow] = useState<DnsModeRow>(DEFAULT_DNS_MODE_ROW)
+    const [nameError, setNameError] = useState(false)
+
+    const handleRowChange = (type: keyof DnsTableRow) => (e: React.ChangeEvent<HTMLInputElement>) => {
+        setRow(prev => {
+            const value = e.target.value
+            type === 'name' && setNameError(value === '')
+            return {...prev, [type]: value}
+        })
+    }
+
+    const handleBack = () => {
+        setAction('')
+        setErrorImportData(false)
+        setDnsModeImportData('')
+        setDnsModeExportData('')
+        setDnsModeChecked([])
+        setUpdateKey(-1)
+    }
+
+    const handleCreate = () => {
+        setAction('create')
+        setRow(DEFAULT_DNS_MODE_ROW)
+    }
+
+    const handleCreateSubmit = async () => {
+        let item: DnsModeRow = {...row}
+
+        item.name = item.name.trim()
+        const isNameEmpty = item.name === ''
+        setNameError(isNameEmpty)
+        if (isNameEmpty) return
+
+        item.note = item.note.trim()
+        item.hash = await hashJson(item)
+
+        dnsModeList.push(item)
+        const ok = await saveDnsModeList(dnsModeList)
+        if (!ok) {
+            showAlertDialog('保存失败')
+            return
+        }
+        setDnsModeList([...dnsModeList])
+        handleBack()
+    }
+
+    const handleImport = () => {
+        setAction('import')
+    }
+
+    const [dnsModeImportData, setDnsModeImportData] = useState('')
+    const [errorImportData, setErrorImportData] = useState(false)
+    const handleDnsModeImportDataChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let value = e.target.value
+        setDnsModeImportData(value)
+        setErrorImportData(value === '')
+    }
+
+    const handleDnsModeImportSubmit = async () => {
+        let s = dnsModeImportData.trim()
+        setErrorImportData(!s)
+        if (!s) return
+
+        const newDnsModeList = [...dnsModeList]
+        let okNum = 0
+        let existNum = 0
+        let errNum = 0
+        let errMsg = ''
+        const arr = s.split('\n')
+        for (let v of arr) {
+            v = v.trim()
+            if (v.length === 0) continue
+
+            if (v.startsWith('drayDns://')) {
+                const base64 = v.substring(10).replace(/#.*$/, '')
+                const decoded = decodeBase64(base64)
+                const data = safeJsonParse(decoded)
+                if (data && typeof data === 'object' && 'hash' in data) {
+                    if (newDnsModeList.some(item => item.hash === data.hash)) {
+                        existNum++
+                    } else {
+                        newDnsModeList.push(data)
+                        okNum++
+                    }
+                } else {
+                    errNum++
+                    errMsg = '解析失败，或数据不正确'
+                }
+            } else {
+                errNum++
+                errMsg = '格式不正确，前缀非 drayDns:// 开头'
+            }
+        }
+
+        if (okNum > 0) {
+            const ok = await saveDnsModeList(newDnsModeList)
+            if (!ok) {
+                showAlertDialog('导入保存失败')
+                return
+            }
+            setDnsModeList(newDnsModeList)
+            handleBack()
+
+            if (existNum > 0 || errNum > 0) {
+                showAlertDialog(`导入成功 ${okNum} 条，已存在 ${existNum} 条，失败 ${errNum} 条`, 'warning')
+            } else {
+                showAlertDialog(`导入成功 ${okNum} 条`, 'success')
+            }
+        } else if (existNum > 0) {
+            showAlertDialog(`导入成功 ${okNum} 条，已存在 ${existNum} 条，失败 ${errNum} 条`, 'warning')
+        } else if (errMsg) {
+            showAlertDialog(errMsg, 'error')
+        }
+    }
+
+    const [dnsModeExportData, setDnsModeExportData] = useState('')
+    const [dnsModeChecked, setDnsModeChecked] = useState<number[]>([])
+    const handleExport = () => {
+        setAction('export')
+
+        let arr = []
+        for (let k = 0; k < dnsModeChecked.length; k++) {
+            const v = dnsModeList[k]
+            if (!v) continue
+            const encoded = 'drayDns://' + encodeBase64(JSON.stringify(v)) + '#' + v.name
+            arr.push(encoded)
+        }
+        setDnsModeExportData(arr.join('\n'))
+    }
+
+    const handleDnsModeCheckedChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setDnsModeChecked(prev => {
+            const value = Number(e.target.value)
+            if (e.target.checked) {
+                return [...prev, value]
+            } else {
+                return prev.filter(item => item !== value)
+            }
+        })
+    }
+
+    const [updateKey, setUpdateKey] = useState(-1)
+    const handleDnsModeUpdate = (key: number) => {
+        setUpdateKey(key)
+    }
+
+    const handleDnsModeViewConf = (key: number) => {
+
+    }
+
+    const handleDnsModeDelete = (key: number, name: string) => {
+        dialogConfirm('确认删除', `确定要删除 "${name}" 吗？`, async () => {
+            if (dnsConfig.mode === key) {
+                showAlertDialog('不允许删除正在使用的模式')
+                return
+            }
+
+            if (dnsModeList.length <= 1) {
+                showAlertDialog('不允许删除所有模式，至少保留一个')
+                return
+            }
+
+            const newList = dnsModeList.filter((_, index) => index !== key) || []
+            const ok = await saveDnsModeList(newList)
+            if (!ok) {
+                showAlertDialog('删除失败')
+            } else {
+                setDnsModeList([...newList])
+                setDnsModeChecked([])
+            }
+        })
+    }
+
+    const [isCopied, setIsCopied] = useState(false)
+    const handleDnsModeCopy = async (content: string) => {
+        const ok = await clipboardWriteText(content)
+        if (!ok) return
+        setIsCopied(true)
+        setTimeout(() => setIsCopied(false), 2000)
+    }
+
+    const {AlertDialogComponent, showAlertDialog} = useAlertDialog()
+    const {DialogComponent, dialogConfirm} = useDialog()
     return (<Stack spacing={2}>
+        <AlertDialogComponent/>
+        <DialogComponent/>
         <BottomNavigation
             sx={{mb: 2}} elevation={5} component={Paper} showLabels
             value={dnsNav}
@@ -73,7 +288,90 @@ export const Dns = () => {
                 </Alert>
             </Stack>
         </> : dnsNav === 1 ? <>
+            <Dialog open={action !== ''}>
+                <Stack spacing={2} sx={{p: 2, minWidth: 580}}>
+                    {action === 'create' ? <>
+                        <Stack spacing={2} component={Card} elevation={5} sx={{p: 1}}>
+                            <TextField fullWidth size="small" label="模式名称"
+                                       error={nameError} helperText={nameError ? "模式名称不能为空" : ""}
+                                       value={row.name} onChange={handleRowChange('name')}/>
+                            <TextField fullWidth size="small" label="模式描述" value={row.note} multiline minRows={2} maxRows={6} onChange={handleRowChange('note')}/>
+                        </Stack>
+                        <div className="flex-between">
+                            <Button variant="contained" color="info" onClick={handleCreateSubmit}>添加</Button>
+                            <Button variant="contained" onClick={handleBack}>取消</Button>
+                        </div>
+                    </> : action === 'import' ? <>
+                        <Stack spacing={2} component={Card} elevation={5} sx={{p: 1, pt: 2}}>
+                            <TextField
+                                size="small" multiline rows={10}
+                                label="导入内容（URI）"
+                                placeholder="每行一条，例如：drayPublicDns://xxxxxx"
+                                error={errorImportData} helperText={errorImportData ? '导入内容不能为空' : ''}
+                                value={dnsModeImportData}
+                                onChange={handleDnsModeImportDataChange}
+                            />
+                        </Stack>
+                        <div className="flex-between">
+                            <Button variant="contained" color="info" onClick={handleDnsModeImportSubmit}>确定</Button>
+                            <Button variant="contained" onClick={handleBack}>取消</Button>
+                        </div>
+                    </> : action === 'export' && <>
+                        <Stack spacing={2} component={Card} elevation={5} sx={{p: 1, pt: 2}}>
+                            <TextField size="small" multiline disabled minRows={10} maxRows={16} label="导出内容（URI）" value={dnsModeExportData}/>
+                        </Stack>
+                        <div className="flex-between">
+                            <div>
+                                <Button variant="contained" color="info" startIcon={<ContentCopyIcon/>} onClick={() => handleDnsModeCopy(dnsModeExportData)}>复制</Button>
+                                {isCopied && <Chip label="复制成功" color="success" size="small" sx={{ml: 2}}/>}
+                            </div>
+                            <Button variant="contained" onClick={handleBack}>取消</Button>
+                        </div>
+                    </>}
+                </Stack>
+            </Dialog>
 
+            <Stack direction="row" spacing={1}>
+                <Button variant="contained" color="secondary" startIcon={<AddIcon/>} onClick={handleCreate}>添加</Button>
+                <Button variant="contained" color="success" startIcon={<FileUploadIcon/>} onClick={handleImport}>导入</Button>
+                {dnsModeChecked.length > 0 && <Button variant="contained" color="warning" startIcon={<FileDownloadIcon/>} onClick={handleExport}>导出</Button>}
+            </Stack>
+            <Stack spacing={1}>
+                {loading ? (
+                    <LoadingCard height="160px" elevation={5}/>
+                ) : dnsModeList.length === 0 ? (
+                    <ErrorCard errorMsg="暂无内容" height="160px" elevation={5}/>
+                ) : (
+                    <TableContainer component={Card} elevation={5}>
+                        <Table>
+                            <TableBody>
+                                {dnsModeList.map((row, key) => (
+                                    <TableRow key={key} sx={{'&:last-child td, &:last-child th': {border: 0}}}>
+                                        <TableCell padding="checkbox">
+                                            <Checkbox value={key} checked={dnsModeChecked.includes(key)} onChange={handleDnsModeCheckedChange}/>
+                                        </TableCell>
+                                        <TableCell component="th" scope="row">
+                                            <Typography gutterBottom variant="h6" component="div">{row.name}</Typography>
+                                            <Typography variant="body2" sx={{color: 'text.secondary'}}>{row.note}</Typography>
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            <Tooltip title="设置" arrow placement="top">
+                                                <IconButton color="primary" onClick={() => handleDnsModeUpdate(key)}><SettingsSuggestIcon/></IconButton>
+                                            </Tooltip>
+                                            <Tooltip title="查看配置" arrow placement="top">
+                                                <IconButton color="info" onClick={() => handleDnsModeViewConf(key)}><VisibilityIcon/></IconButton>
+                                            </Tooltip>
+                                            <Tooltip title="删除" arrow placement="top">
+                                                <IconButton color="error" onClick={() => handleDnsModeDelete(key, row.name)}><DeleteIcon/></IconButton>
+                                            </Tooltip>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                )}
+            </Stack>
         </> : dnsNav === 2 && <DnsTable/>}
     </Stack>)
 }
