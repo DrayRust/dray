d4f93fa920eb224b84ec038f3c532c599aabc4c8
Dray <drayrust123@gmail.com>
2025-04-22 17:28:06 +0800

    changes

--- src-tauri/src/fs.rs
+++ src-tauri/src/fs.rs
@@ -3,6 +3,11 @@ use std::fs;
 use std::io::Write;
 
 pub fn save_text_file(path: &str, content: &str) -> bool {
+    if path.contains("../") || path.contains("..\\") {
+        error!("Blocked potentially malicious path: {}", path);
+        return false;
+    }
+
     match fs::File::create(path) {
         Ok(mut file) => {
             if let Err(e) = file.write_all(content.as_bytes()) {
