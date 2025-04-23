97dafca4db35de5d0419d6db57fac85c64c45fb1
Dray <drayrust123@gmail.com>
2025-04-23 21:07:08 +0800

    changes

--- src/view/Rule.tsx
+++ src/view/Rule.tsx
@@ -11,6 +11,7 @@ import FlightIcon from '@mui/icons-material/Flight'
 import BlockIcon from '@mui/icons-material/Block'
 import SettingsIcon from '@mui/icons-material/Settings'
 
+import { LoadingCard } from "../component/useCard.tsx"
 import { useChip } from "../component/useChip.tsx"
 import { RuleAdvanced } from './RuleAdvanced.tsx'
 import { Dns } from './Dns.tsx'
@@ -24,6 +25,7 @@ import { rayRuleChange } from "../util/ray.ts"
 const Rule: React.FC<NavProps> = ({setNavState}) => {
     useEffect(() => setNavState(3), [setNavState])
 
+    const [loading, setLoading] = useState(true)
     const [ruleMode, setRuleMode] = useState('route')
     const [ruleType, setRuleType] = useState(0)
     const [ruleConfig, setRuleConfig] = useState<RuleConfig>(DEFAULT_RULE_CONFIG)
@@ -35,6 +37,7 @@ const Rule: React.FC<NavProps> = ({setNavState}) => {
 
             const newRuleDomain = await readRuleDomain() as RuleDomain
             if (newRuleDomain) setRuleDomain({...DEFAULT_RULE_DOMAIN, ...newRuleDomain})
+            setLoading(false)
         })()
     }, [])
 
@@ -99,7 +102,9 @@ const Rule: React.FC<NavProps> = ({setNavState}) => {
     }
 
     const {ChipComponent, showChip} = useChip()
-    return (<>
+    return loading ? (
+        <LoadingCard/>
+    ) : (<>
         <Paper elevation={5} sx={{p: 1, borderRadius: 2, height: 'calc(100vh - 20px)', overflow: 'visible'}}>
             <div className="flex-center p1">
                 <ToggleButtonGroup exclusive value={ruleMode} onChange={(_, v) => v && setRuleMode(v)}>
@@ -108,65 +113,67 @@ const Rule: React.FC<NavProps> = ({setNavState}) => {
                 </ToggleButtonGroup>
             </div>
             <Card sx={{p: 2, maxWidth: '800px', maxHeight: 'calc(100% - 56px)', m: 'auto', overflow: 'auto'}}>
-                {ruleMode === 'route' && (<div className="flex-column gap2">
-                    <div className="flex-between">
-                        <Typography variant="body1" sx={{pl: 1}}>全局代理</Typography>
-                        <Switch checked={ruleConfig.globalProxy} onChange={(_, checked) => handleGlobalProxy(checked)}/>
-                    </div>
-                    {!ruleConfig.globalProxy && (<>
-                        <BottomNavigation
-                            sx={{mb: 2}}
-                            showLabels
-                            component={Paper}
-                            elevation={5}
-                            value={ruleType}
-                            onChange={(_, v) => setRuleType(v)}>
-                            <BottomNavigationAction label="代理" icon={<SendIcon/>}/>
-                            <BottomNavigationAction label="直连" icon={<FlightIcon/>}/>
-                            <BottomNavigationAction label="阻止" icon={<BlockIcon/>}/>
-                        </BottomNavigation>
-
-                        {ruleType === 0 ? (<>
-                            <Alert variant="filled" severity="warning">
-                                通过第三方服务器访问网络，适合访问国外网站或需要隐藏真实 IP 的场景。
-                            </Alert>
-                            <TextField
-                                variant="outlined" fullWidth multiline rows={6}
-                                label="请填写域名(每行一条)"
-                                value={ruleDomain.proxy}
-                                onChange={handleDomainChange('proxy')}
-                                placeholder="例如：google.com"/>
-                        </>) : ruleType === 1 ? (<>
-                            <Alert variant="filled" severity="success">
-                                直接连接网络，不经过任何代理服务器，适合访问国内网站。
-                            </Alert>
-                            <TextField
-                                variant="outlined" fullWidth multiline rows={6}
-                                label="请填写域名(每行一条)"
-                                value={ruleDomain.direct}
-                                onChange={handleDomainChange('direct')}
-                                placeholder="例如：baidu.com"/>
-                        </>) : ruleType === 2 && (<>
-                            <Alert variant="filled" severity="error">
-                                阻止访问某些网站或服务，适合屏蔽广告、恶意网站或不希望访问的内容。
-                            </Alert>
-                            <TextField
-                                variant="outlined" fullWidth multiline rows={6}
-                                label="请填写域名(每行一条)"
-                                value={ruleDomain.block}
-                                onChange={handleDomainChange('block')}
-                                placeholder="例如：fakebanklogin.com"/>
-                        </>)}
-
+                {ruleMode === 'route' && (
+                    <div className="flex-column gap2">
                         <div className="flex-between">
-                            <Stack direction="row" spacing={2} sx={{justifyContent: "flex-start", alignItems: "center"}}>
-                                <Button variant="contained" color="info" onClick={handleSaveRuleDomain}>确认</Button>
-                                <ChipComponent/>
-                            </Stack>
-                            <Button variant="contained" onClick={handleOpenAdvanced} startIcon={<SettingsIcon/>}>高级</Button>
+                            <Typography variant="body1" sx={{pl: 1}}>全局代理</Typography>
+                            <Switch checked={ruleConfig.globalProxy} onChange={(_, checked) => handleGlobalProxy(checked)}/>
                         </div>
-                    </>)}
-                </div>)}
+                        {!ruleConfig.globalProxy && (<>
+                            <BottomNavigation
+                                sx={{mb: 2}}
+                                showLabels
+                                component={Paper}
+                                elevation={5}
+                                value={ruleType}
+                                onChange={(_, v) => setRuleType(v)}>
+                                <BottomNavigationAction label="代理" icon={<SendIcon/>}/>
+                                <BottomNavigationAction label="直连" icon={<FlightIcon/>}/>
+                                <BottomNavigationAction label="阻止" icon={<BlockIcon/>}/>
+                            </BottomNavigation>
+
+                            {ruleType === 0 ? (<>
+                                <Alert variant="filled" severity="warning">
+                                    通过第三方服务器访问网络，适合访问国外网站或需要隐藏真实 IP 的场景。
+                                </Alert>
+                                <TextField
+                                    variant="outlined" fullWidth multiline rows={6}
+                                    label="请填写域名(每行一条)"
+                                    value={ruleDomain.proxy}
+                                    onChange={handleDomainChange('proxy')}
+                                    placeholder="例如：google.com"/>
+                            </>) : ruleType === 1 ? (<>
+                                <Alert variant="filled" severity="success">
+                                    直接连接网络，不经过任何代理服务器，适合访问国内网站。
+                                </Alert>
+                                <TextField
+                                    variant="outlined" fullWidth multiline rows={6}
+                                    label="请填写域名(每行一条)"
+                                    value={ruleDomain.direct}
+                                    onChange={handleDomainChange('direct')}
+                                    placeholder="例如：baidu.com"/>
+                            </>) : ruleType === 2 && (<>
+                                <Alert variant="filled" severity="error">
+                                    阻止访问某些网站或服务，适合屏蔽广告、恶意网站或不希望访问的内容。
+                                </Alert>
+                                <TextField
+                                    variant="outlined" fullWidth multiline rows={6}
+                                    label="请填写域名(每行一条)"
+                                    value={ruleDomain.block}
+                                    onChange={handleDomainChange('block')}
+                                    placeholder="例如：fakebanklogin.com"/>
+                            </>)}
+
+                            <div className="flex-between">
+                                <Stack direction="row" spacing={2} sx={{justifyContent: "flex-start", alignItems: "center"}}>
+                                    <Button variant="contained" color="info" onClick={handleSaveRuleDomain}>确认</Button>
+                                    <ChipComponent/>
+                                </Stack>
+                                <Button variant="contained" onClick={handleOpenAdvanced} startIcon={<SettingsIcon/>}>高级</Button>
+                            </div>
+                        </>)}
+                    </div>
+                )}
 
                 {ruleMode === 'dns' && <Dns/>}
             </Card>
