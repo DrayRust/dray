3d7fb8751167756db437930e716b31a71f973e6e
Dray <drayrust123@gmail.com>
2025-04-24 11:14:12 +0800

    changes

--- src/util/serverSpeed.ts
+++ src/util/serverSpeed.ts
@@ -32,12 +32,13 @@ export async function serverSpeedTest(server: ServerRow, appDir: string, port: n
     await sleep(500)
     const startTime = Date.now()
 
+    // 目前测试 http 比 https 快 100-500ms
     // https://www.gstatic.com/generate_204
     // https://www.google.com/generate_204
     // https://cp.cloudflare.com/generate_204
-    // https://www.msftconnecttest.com/connecttest.txt
     // https://captive.apple.com/hotspot-detect.htm
-    await fetchProxyGet("https://www.gstatic.com/generate_204", `socks5://127.0.0.1:${port}`)
+    // http://www.msftconnecttest.com/connecttest.txt
+    await fetchProxyGet('http://www.gstatic.com/generate_204', `socks5://127.0.0.1:${port}`)
 
     return Date.now() - startTime
 }
