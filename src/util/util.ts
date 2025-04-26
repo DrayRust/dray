8424f09500c1e28aa7cdbf5f47b0b30803fbb9c9
Dray <drayrust123@gmail.com>
2025-04-26 22:31:05 +0800

    changes

--- src/util/util.ts
+++ src/util/util.ts
@@ -49,6 +49,20 @@ export const calcPct = (used: number, total: number): string => {
     return `${percentage}%`
 }
 
+export const formatTimestamp = (timestamp: number): string => {
+    timestamp = Number(timestamp) || 0
+    if (timestamp === 0) return '-'
+
+    const date = new Date(timestamp * 1000)
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
+    const hours = String(date.getHours()).padStart(2, '0')
+    const minutes = String(date.getMinutes()).padStart(2, '0')
+    const seconds = String(date.getSeconds()).padStart(2, '0')
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
+
 const padZero = (num: number) => (Number(num) || 0).toString().padStart(2, '0')
 
 export const formatTime = (seconds: number): string => {
--- src/view/Process.tsx
+++ src/view/Process.tsx
@@ -1,7 +1,36 @@
-import { Box, Paper, IconButton } from '@mui/material'
+import { useState, useEffect, useRef } from 'react'
+import {
+    Box, Card, IconButton, styled,
+    TableContainer, Table, TableBody, TableCell, TableHead, TableRow,
+} from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
+import { useDebounce } from "../hook/useDebounce.ts"
+import { getProcessesJson } from "../util/invoke.ts"
+import { useVisibility } from "../hook/useVisibility.ts"
+import { formatFloat, formatTimestamp, sizeToUnit } from "../util/util.ts"
 
 export const Process = ({handleClose}: { handleClose: () => void }) => {
+    const [processes, setProcesses] = useState<any>([])
+
+    const loadData = useDebounce(async () => {
+        let r = await getProcessesJson()
+        if (r) setProcesses(r)
+    }, 300)
+
+    // 可见时，自动刷新数据
+    const intervalRef = useRef<number>(0)
+    const isVisibility = useVisibility()
+    useEffect(() => {
+        loadData()
+        if (isVisibility) intervalRef.current = setInterval(loadData, 5000)
+        return () => clearInterval(intervalRef.current)
+    }, [isVisibility])
+
+    const TableCellN = styled(TableCell)({
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+    })
 
     return (
         <Box sx={{p: 1}}>
@@ -11,16 +40,36 @@ export const Process = ({handleClose}: { handleClose: () => void }) => {
                 <CloseIcon/>
             </IconButton>
 
-            <Paper elevation={2} sx={{
-                width: '100%',
-                height: `calc(100vh - 20px)`,
-                borderRadius: 3,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}>
-                <h1 onClick={handleClose}>Process</h1>
-            </Paper>
+            <TableContainer elevation={2} component={Card}>
+                <Table size="small">
+                    <TableHead>
+                        <TableRow>
+                            <TableCellN>PID</TableCellN>
+                            <TableCellN>用户</TableCellN>
+                            <TableCellN>状态</TableCellN>
+                            <TableCellN>内存</TableCellN>
+                            <TableCellN>CPU</TableCellN>
+                            <TableCellN>运行时间</TableCellN>
+                            <TableCellN>进程名称</TableCellN>
+                            <TableCellN>程序路径</TableCellN>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {processes?.length > 0 && processes.map((row: any, key: number) => (
+                            <TableRow key={key} sx={{'&:last-child td, &:last-child th': {border: 0}}}>
+                                <TableCellN>{row.pid}</TableCellN>
+                                <TableCellN>{row.user}</TableCellN>
+                                <TableCellN>{row.status}</TableCellN>
+                                <TableCellN>{sizeToUnit(row.memory)}</TableCellN>
+                                <TableCellN>{formatFloat(row.cpu_usage, 1)}%</TableCellN>
+                                <TableCellN>{formatTimestamp(row.start_time)}</TableCellN>
+                                <TableCellN>{row.name}</TableCellN>
+                                <TableCellN>{row.exe}</TableCellN>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Box>
     )
 }
