221604d1130cc33d6d9380e68b58266f087a1739
Dray <drayrust123@gmail.com>
2025-04-26 11:04:15 +0800

    changes

--- src/hook/useWindowFocused.ts
+++ src/hook/useWindowFocused.ts
@@ -6,18 +6,13 @@ export const useWindowFocused = () => {
     useEffect(() => {
         const handleFocus = () => setIsWindowFocused(true)
         const handleBlur = () => setIsWindowFocused(false)
-        const handleVisibilityChange = () => {
-            setIsWindowFocused(document.visibilityState === 'visible')
-        }
 
         window.addEventListener('focus', handleFocus)
         window.addEventListener('blur', handleBlur)
-        document.addEventListener('visibilitychange', handleVisibilityChange)
 
         return () => {
             window.removeEventListener('focus', handleFocus)
             window.removeEventListener('blur', handleBlur)
-            document.removeEventListener('visibilitychange', handleVisibilityChange)
         }
     }, [])
 
