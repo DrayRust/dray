4b5b8c9640754fe9913bc2719e777f7460c7eb66
Dray <drayrust123@gmail.com>
2025-05-03 09:38:21 +0800

    changes

--- src/util/invoke.ts
+++ src/util/invoke.ts
@@ -52,36 +52,24 @@ export async function getDrayAppDir(): Promise<string> {
     return invokeString('get_dray_app_dir')
 }
 
-export async function fetchGet(url: string, isProxy: boolean = false) {
-    return await safeInvoke('fetch_get', {url, isProxy})
-}
-
-export async function fetchProxyGet(url: string, proxyUrl: string) {
-    return await safeInvoke('fetch_get_with_proxy', {url, proxyUrl})
-}
-
-export async function downloadLargeFile(url: string, filepath: string, timeout: number = 60 * 30) {
-    return await safeInvoke('download_large_file', {url, filepath, timeout})
-}
-
 export function restartRay() {
     return invokeBool('restart_ray')
 }
 
 export async function checkPortAvailable(port: number) {
-    return await invokeBool('check_port_available', {port})
+    return invokeBool('check_port_available', {port})
 }
 
 export async function startSpeedTestServer(port: number, filename: string) {
-    return await invokeBool('start_speed_test_server', {port, filename})
+    return invokeBool('start_speed_test_server', {port, filename})
 }
 
 export async function stopSpeedTestServer(port: number) {
-    return await invokeBool('stop_speed_test_server', {port})
+    return invokeBool('stop_speed_test_server', {port})
 }
 
 export async function readAppConfig(): Promise<AppConfig | undefined> {
-    return await safeInvoke('get_config_json')
+    return safeInvoke('get_config_json')
 }
 
 export function setAppConfig(cmd: string, value: string | number | boolean) {
@@ -92,47 +80,47 @@ export function setAppConfig(cmd: string, value: string | number | boolean) {
 }
 
 export async function readRayConfig(): Promise<any> {
-    return await safeInvoke('read_ray_config')
+    return safeInvoke('read_ray_config')
 }
 
 export async function saveRayConfig(content: any) {
-    return await invokeBool('save_ray_config', {content: jsonStringify(content)})
+    return invokeBool('save_ray_config', {content: jsonStringify(content)})
 }
 
 export async function saveProxyPac(content: string) {
-    return await invokeBool('save_proxy_pac', {content})
+    return invokeBool('save_proxy_pac', {content})
 }
 
 export async function saveTextFile(path: string, content: string) {
-    return await invokeBool('save_text_file', {path, content})
+    return invokeBool('save_text_file', {path, content})
 }
 
 export async function openWebServerDir() {
-    return await invokeBool('open_web_server_dir')
+    return invokeBool('open_web_server_dir')
 }
 
 export async function clearLogAll() {
-    return await invokeBool('clear_log_all')
+    return invokeBool('clear_log_all')
 }
 
 export async function readLogList(): Promise<LogList | undefined> {
-    return await safeInvoke('read_log_list')
+    return safeInvoke('read_log_list')
 }
 
 export async function readLogFile(filename: string, reverse: boolean = true, start: number = -1): Promise<LogContent | undefined> {
-    return await safeInvoke('read_log_file', {filename, reverse, start})
+    return safeInvoke('read_log_file', {filename, reverse, start})
 }
 
 async function readConf(filename: string) {
-    return await safeInvoke('read_conf', {filename})
+    return safeInvoke('read_conf', {filename})
 }
 
 async function saveConf(filename: string, content: any) {
-    return await invokeBool('save_conf', {filename, content: jsonStringify(content)})
+    return invokeBool('save_conf', {filename, content: jsonStringify(content)})
 }
 
 export async function saveSpeedTestConf(filename: string, content: any) {
-    return await invokeBool('save_speed_test_conf', {filename, content: jsonStringify(content)})
+    return invokeBool('save_speed_test_conf', {filename, content: jsonStringify(content)})
 }
 
 export async function readRayCommonConfig(): Promise<RayCommonConfig | undefined> {
@@ -243,6 +231,18 @@ export async function killProcessByPid(pid: number) {
     return invokeBool('kill_process_by_pid', {pid})
 }
 
+export async function fetchGet(url: string, isProxy: boolean = false) {
+    return safeInvoke('fetch_get', {url, isProxy})
+}
+
+export async function fetchProxyGet(url: string, proxyUrl: string) {
+    return safeInvoke('fetch_get_with_proxy', {url, proxyUrl})
+}
+
+export async function downloadLargeFile(url: string, filepath: string, timeout: number = 60 * 30) {
+    return safeInvoke('download_large_file', {url, filepath, timeout})
+}
+
 export async function pingTest(url: string, userAgent: string, count: number, timeout: number = 5) {
     return safeInvoke('ping_test', {url, userAgent, count, timeout})
 }
@@ -251,8 +251,8 @@ export async function jitterTest(url: string, userAgent: string, count: number, 
     return safeInvoke('jitter_test', {url, userAgent, count, timeout})
 }
 
-export async function downloadSpeedTest(url: string, userAgent: string, timeout: number = 60 * 20) {
-    return safeInvoke('download_speed_test', {url, userAgent, timeout})
+export async function downloadSpeedTest(url: string, proxyUrl: string, userAgent: string, timeout: number = 60 * 20) {
+    return safeInvoke('download_speed_test', {url, proxyUrl, userAgent, timeout})
 }
 
 export async function uploadSpeedTest(url: string, userAgent: string, size: number, timeout: number = 60 * 20) {
