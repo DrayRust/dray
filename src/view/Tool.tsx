a9342c659682fff7971f17254c655ec2c6ddcfc5
Dray <drayrust123@gmail.com>
2025-05-03 15:45:39 +0800

    changes

--- src/view/Tool.tsx
+++ src/view/Tool.tsx
@@ -5,33 +5,33 @@ import WysiwygIcon from '@mui/icons-material/Wysiwyg'
 import SpeedIcon from '@mui/icons-material/Speed'
 import HttpIcon from '@mui/icons-material/Http'
 
-import TerminalCmd from "./TerminalCmd.tsx"
 import SysInfo from "./SysInfo.tsx"
-import SpeedTest from "./SpeedTest.tsx"
+import TerminalCmd from "./TerminalCmd.tsx"
 import HttpTest from "./HttpTest.tsx"
+import SpeedTest from "./SpeedTest.tsx"
 
 const Tool: React.FC<NavProps> = ({setNavState}) => {
     useEffect(() => setNavState(5), [setNavState])
 
-    const [action, setAction] = useState('term')
+    const [action, setAction] = useState('system')
 
     return (
         <Paper elevation={5} sx={{py: 1, borderRadius: 2, height: 'calc(100vh - 20px)'}}>
             <div className="flex-center p1">
                 <ToggleButtonGroup exclusive value={action} onChange={(_, v) => v && setAction(v)}>
-                    <ToggleButton value="term"><TerminalIcon sx={{mr: 1}}/>终端命令</ToggleButton>
                     <ToggleButton value="system"><WysiwygIcon sx={{mr: 1}}/>系统信息</ToggleButton>
-                    <ToggleButton value="speed"><SpeedIcon sx={{mr: 1}}/>网速测试</ToggleButton>
+                    <ToggleButton value="term"><TerminalIcon sx={{mr: 1}}/>终端命令</ToggleButton>
                     <ToggleButton value="http"><HttpIcon sx={{mr: 1}}/>请求测试</ToggleButton>
+                    <ToggleButton value="speed"><SpeedIcon sx={{mr: 1}}/>网速测试</ToggleButton>
                 </ToggleButtonGroup>
             </div>
 
             <Box sx={{maxHeight: 'calc(100% - 56px)', overflow: 'auto'}}>
                 <Card sx={{p: 2, maxWidth: '800px', m: 'auto'}}>
-                    {action === 'term' && (<TerminalCmd/>)}
                     {action === 'system' && (<SysInfo/>)}
-                    {action === 'speed' && (<SpeedTest/>)}
+                    {action === 'term' && (<TerminalCmd/>)}
                     {action === 'http' && (<HttpTest/>)}
+                    {action === 'speed' && (<SpeedTest/>)}
                 </Card>
             </Box>
         </Paper>
