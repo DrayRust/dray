01db691258da5d8637620e5b8d44bb9154476a7d
Dray <drayrust123@gmail.com>
2025-05-07 13:40:40 +0800

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+document.addEventListener('contextmenu', e => e.preventDefault())
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App/>
-	</StrictMode>,
+    <StrictMode>
+        <App/>
+    </StrictMode>,
 )
